8733885c95cf2421fe7c8e52ab7356b4ffc94f0a
Enrico Weißenrieder <e.weissenrieder94@gmail.com>
2023-04-03 10:53:55 +0200

    addec basic UI, state manager and AddCalories component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,20 @@
+import AddCalories from "../components/AddCalories";
+import useCalorieStore from "../utils/useCalorieStore";
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
 export default function HomePage() {
+  const { dailyCount, addDailyCount } = useCalorieStore();
+
   return (
-    <div>
-      <h1>Hello from Next.js</h1>
-    </div>
+    <StyledDiv>
+      <h1>{dailyCount}</h1>
+      <AddCalories addDailyCount={addDailyCount} />
+    </StyledDiv>
   );
 }

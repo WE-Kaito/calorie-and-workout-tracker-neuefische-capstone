7cf3182e19822f31139c43fb2994007b18e66d86
Enrico Weißenrieder <e.weissenrieder94@gmail.com>
2023-04-13 17:24:16 +0200

    fix

--- pages/dishes/[index].js
+++ pages/dishes/[index].js
@@ -51,21 +51,21 @@ export default function DishDetails() {
     event.target.reset();
   }
 
-  return (
-    <Wrapper>
-      <DeleteButton
-        onClick={() => {
-          deleteDish(dishes[index]);
-          router.push("/dishes");
-        }}
-      >
-        DELETE ENTRY
-      </DeleteButton>
-      <Link href="/">
-        <BackButton>🔙</BackButton>
-      </Link>
-      <List invisible={formVisibility}>
-        {dishes[0] && (
+  if (dishes[0])
+    return (
+      <Wrapper>
+        <DeleteButton
+          onClick={() => {
+            deleteDish(dishes[index]);
+            router.push("/dishes");
+          }}
+        >
+          DELETE ENTRY
+        </DeleteButton>
+        <Link href="/">
+          <BackButton>🔙</BackButton>
+        </Link>
+        <List invisible={formVisibility}>
           <DetailsList>
             <Detail>
               <DishHeadline>{dishes[index].meal}</DishHeadline>
@@ -76,85 +76,85 @@ export default function DishDetails() {
             <Detail>{`Carbs: ${dishes[index].carbs} g`}</Detail>
             <Detail>{`Notes: ${dishes[index].notes}`}</Detail>
           </DetailsList>
-        )}
-        <DetailsBackButton href="/dishes">←</DetailsBackButton>
-        <DetailsEditButton
-          onClick={() => {
-            toggleFormVisibility(!formVisibility);
-          }}
-        >
-          EDIT
-        </DetailsEditButton>
-      </List>
 
-      <AddDishForm
-        visible={formVisibility}
-        onSubmit={(event) => {
-          handleSubmit(event);
-        }}
-      >
-        <Label for="meal">Your Meal:</Label>
-        <Input
-          id="meal"
-          name="meal"
-          type="text"
-          required
-          placeholder={`${dishes[index].meal}`}
-        ></Input>
-
-        <Label for="calories">{`Calories (kcal):`}</Label>
-        <Input
-          id="calories"
-          name="calories"
-          type="number"
-          required
-          placeholder={`${dishes[index].calories}`}
-        ></Input>
-
-        <Label for="mass">{`Mass (g):`}</Label>
-        <Input
-          id="mass"
-          name="mass"
-          type="number"
-          placeholder={`${dishes[index].mass}`}
-        ></Input>
-
-        <Label for="proteins">{`Proteins (g):`}</Label>
-        <Input
-          if="proteins"
-          name="proteins"
-          type="number"
-          placeholder={`${dishes[index].proteins}`}
-        ></Input>
-
-        <Label for="carbs">{`Carbs (g):`}</Label>
-        <Input
-          id="carbs"
-          name="carbs"
-          type="number"
-          placeholder={`${dishes[index].carbs}`}
-        ></Input>
-
-        <Label for="notes">Notes:</Label>
-        <Input
-          id="notes"
-          name="notes"
-          type="text"
-          placeholder={`${dishes[index].notes}`}
-          style={{ textAlign: "center" }}
-        ></Input>
-        <ButtonWrapper>
-          <CloseFormButton
-            type="button"
+          <DetailsBackButton href="/dishes">←</DetailsBackButton>
+          <DetailsEditButton
             onClick={() => {
-              toggleFormVisibility(false);
+              toggleFormVisibility(!formVisibility);
             }}
           >
-            close
-          </CloseFormButton>
-          <SubmitButton>save</SubmitButton>
-        </ButtonWrapper>
-      </AddDishForm>
-    </Wrapper>
-  );
+            EDIT
+          </DetailsEditButton>
+        </List>
+
+        <AddDishForm
+          visible={formVisibility}
+          onSubmit={(event) => {
+            handleSubmit(event);
+          }}
+        >
+          <Label for="meal">Your Meal:</Label>
+          <Input
+            id="meal"
+            name="meal"
+            type="text"
+            required
+            placeholder={`${dishes[index].meal}`}
+          ></Input>
+
+          <Label for="calories">{`Calories (kcal):`}</Label>
+          <Input
+            id="calories"
+            name="calories"
+            type="number"
+            required
+            placeholder={`${dishes[index].calories}`}
+          ></Input>
+
+          <Label for="mass">{`Mass (g):`}</Label>
+          <Input
+            id="mass"
+            name="mass"
+            type="number"
+            placeholder={`${dishes[index].mass}`}
+          ></Input>
+
+          <Label for="proteins">{`Proteins (g):`}</Label>
+          <Input
+            if="proteins"
+            name="proteins"
+            type="number"
+            placeholder={`${dishes[index].proteins}`}
+          ></Input>
+
+          <Label for="carbs">{`Carbs (g):`}</Label>
+          <Input
+            id="carbs"
+            name="carbs"
+            type="number"
+            placeholder={`${dishes[index].carbs}`}
+          ></Input>
+
+          <Label for="notes">Notes:</Label>
+          <Input
+            id="notes"
+            name="notes"
+            type="text"
+            placeholder={`${dishes[index].notes}`}
+            style={{ textAlign: "center" }}
+          ></Input>
+          <ButtonWrapper>
+            <CloseFormButton
+              type="button"
+              onClick={() => {
+                toggleFormVisibility(false);
+              }}
+            >
+              close
+            </CloseFormButton>
+            <SubmitButton>save</SubmitButton>
+          </ButtonWrapper>
+        </AddDishForm>
+      </Wrapper>
+    );
 }

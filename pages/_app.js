9d71681a8d2937081923c4f7f85b448f66788a3d
Enrico Weißenrieder <e.weissenrieder94@gmail.com>
2023-05-02 05:58:14 +0200

    added title

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <title>Calorie & Workout Tracker</title>
         <link rel="shortcut icon" href="/favicon.ico" />
         <meta
           name="viewport"

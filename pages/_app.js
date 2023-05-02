6a4640e172cecb918ef2042c75287d6ef2b3df2c
Enrico Weißenrieder <e.weissenrieder94@gmail.com>
2023-05-02 05:30:24 +0200

    added favicon

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
